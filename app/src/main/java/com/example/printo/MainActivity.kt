3796eccceaafc8ca524a925f9cecf33bd7ba54d7
atulraj <increedBull@protonmail.com>
2021-08-05 16:27:08 +0530

    first setup

---
+++ app/src/main/java/com/example/printo/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.printo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+
+/*
+* 1. Start the Hotspot.
+* 2. Start the ftp server.
+* 3. Add auto deletion of file after selected time.
+* */
